ff608fd79676de526d4f9fde9eb02e2b2ca41f8f
Albert <albertmontes64@gmail.com>
2023-03-21 23:25:40 +0100

    Imatge fons, boto login, 3 activitis

--- app/src/main/java/com/example/residueixappmobil/MainActivity.java
+++ app/src/main/java/com/example/residueixappmobil/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.residueixappmobil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void loginButton(View view) {
+        Intent intentPantallaLogin = new Intent(this,PantallaLogin.class);
+        intentPantallaLogin.setAction(Intent.ACTION_SEND);
+        startActivity(intentPantallaLogin);
     }
 }
