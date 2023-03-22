e8f9f8e2cdedbd982fe0ab4ede221f824967017e
Albert <albertmontes64@gmail.com>
2023-03-22 21:44:46 +0100

    Primers pasos login

---
+++ app/src/main/java/com/example/residueixappmobil/utils/Eines.java
@@ -0,0 +1,34 @@
+package com.example.residueixappmobil.utils;
+
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.residueixappmobil.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Eines extends AppCompatActivity {
+
+
+    public void tornar_enrere(View view) {
+        Button botoEnrere = findViewById(R.id.boto_enrere);
+        botoEnrere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    public boolean validarEmail(String email){
+        // Patró
+        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)@" + "[A-Za-z0-9-]+(\.[A-Za-z0-9]+)(\.[A-Za-z]{2,})$");
+        Matcher mather = pattern.matcher(email);
+        return mather.find();
+    }
+
+    Hola
+
+}
