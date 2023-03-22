d263a54a2e98bcc73ade01d704cf606eaec75b55
Albert <albertmontes64@gmail.com>
2023-03-22 02:41:13 +0100

    next steps

---
+++ app/src/main/java/com/example/residueixappmobil/QuiSomActivity.java
@@ -0,0 +1,12 @@
+package com.example.residueixappmobil;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuiSomActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qui_som);
+    }
+}
